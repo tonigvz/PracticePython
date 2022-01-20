deb314faad2056171f794ed9ad14fccf252bc1ce
antonia <goagaantonia@gmail.com>
2022-01-20 08:59:24 +0200

    laptop/array.diff

--- array.diff/arraydiff.py
+++ array.diff/arraydiff.py
@@ -1,12 +1,10 @@
 #https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
 a = [1,2,2,2,3]
 b = [2]
-res = []
 def array_diff(a,b):
+    res = []
     for i in a:
-        if i in b:
-            pass
-        else:
+        if i not in b:
             res.append(i)
     return res
     #return [i for i in a if i not in b]
