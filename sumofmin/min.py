df728cff2ce068d9107421c65fd317f618a10ec1
antonia <goagaantonia@gmail.com>
2022-01-24 22:19:14 +0200

    laptop/sumofmin

--- sumofmin/min.py
+++ sumofmin/min.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/5d5ee4c35162d9001af7d699/train/python
-def sum_of_minimums(numbers):
-    pass
+def sum_of_minimums(x):
+    sum = 0
+    for i in range(len(x)):
+        sum += min(x[i])
+    print(sum)
 
 
-printsum_of_minimums([[1, 2, 3, 4, 5], [5, 6, 7, 8, 9], [20, 21, 34, 56, 100]])
+(sum_of_minimums([[1, 2, 3, 4, 5], [5, 6, 7, 8, 9], [20, 21, 34, 56, 100]]))
