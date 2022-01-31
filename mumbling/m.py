9429566a2be0261204435683558aabdaa25f65a9
antonia <goagaantonia@gmail.com>
2022-01-31 15:28:14 +0200

    pc/mumbling

--- mumbling/m.py
+++ mumbling/m.py
@@ -3,7 +3,6 @@ def accum(s):
     res = ''
     for i, c in enumerate(s):
         for j in range(i+1):
-            print(i, c, j)
             if j == 0:
                 res += c.upper()
             else:
