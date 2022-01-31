01229671b79f1049ca0fc2467ae8806844ee193e
antonia <goagaantonia@gmail.com>
2022-02-01 00:54:25 +0200

    pc/findcapitals delete

--- findcapitals/cap.py
+++
@@ -1,8 +0,0 @@
-# https://www.codewars.com/kata/53573877d5493b4d6e00050c/train/python
-def capital(capitals):
-    for i in capitals:
-        print("the capital of ", i["state"], " is", i["capital"])
-
-
-state_capitals = [{'state': 'Maine', 'capital': 'Augusta'}]
-print(capital(state_capitals))
