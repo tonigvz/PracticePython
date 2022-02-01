4d8fc6fa620a7dfecc7b23c76a00ebe3b6a22dfd
antonia <goagaantonia@gmail.com>
2022-02-01 22:59:25 +0200

    pc/paulmisery

---
+++ paulmisery/paul.py
@@ -0,0 +1,25 @@
+# https://www.codewars.com/kata/57ee31c5e77282c24d000024/train/python
+from collections import Counter
+
+
+def paul(x):
+    kata, Petes_kata, life, eating = 5, 10, 0, 1
+    countl, counte, countk, countp = 0, 0, 0, 0
+    for i in x:
+        countl = x.count("life")
+        counte = x.count("eating")
+        countk = x.count("kata")
+        countp = x.count("Petes kata")
+    sum = ((countl * life) + (counte * eating) +
+           (countk * kata) + (countp * Petes_kata))
+    if sum < 40:
+        return 'Super happy!'
+    elif sum < 70 >= 40:
+        return 'Happy!'
+    elif sum < 100 >= 70:
+        return 'Sad!'
+    else:
+        return 'Miserable!'
+
+
+print(paul(['life', 'eating', 'life']))
