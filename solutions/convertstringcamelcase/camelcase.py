bde39140ad12f9dc902a00ba66a3679aa33bc8d6
antonia <goagaantonia@gmail.com>
2022-02-06 13:50:58 +0200

    pc/camelcase

--- solutions/convertstringcamelcase/camelcase.py
+++ solutions/convertstringcamelcase/camelcase.py
@@ -1,17 +1,16 @@
 # https://www.codewars.com/kata/517abf86da9663f1d2000003
 def to_camel_case(text):
-    s = text.split('-')
-    if s[0].istitle():
-        return ''.join(s)
-    else:
-        for i in range(len(s)):
-            if i == 0:
-                continue
-            else:
-                s[i] = s[i].capitalize()
-
+    if len(text) == 0:
+        return text
+    text = text.replace("_", "-")
+    s = text.split("-")
+    for i in range(len(s)):
+        if i == 0:
+            continue
+        else:
+            s[i] = s[i].capitalize()
     res = "".join(i for i in s)
     return res
 
 
-print(to_camel_case("the-Stealth-Warrior"))
+print(to_camel_case("the_stealth_warrior"))
