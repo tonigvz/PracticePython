2b1454c74c43f3a93ffb0aaebe896f431b65b1ed
Antonia <goagaantonia@gmail.com>
2022-02-27 22:56:07 +0200

    laptop/arithmeticfunction

---
+++ solutions/arithmetic/farithmetic.py
@@ -0,0 +1,23 @@
+# https://www.codewars.com/kata/583f158ea20cfcbeb400000a
+def arithmetic(a, b, operator):
+    if operator == "add":
+        return a + b
+    elif operator == "subtract":
+        return a - b
+    elif operator == "multiply":
+        return a * b
+    else:
+        return a / b
+
+
+"""
+def arithmetic(a, b, operator):
+    return {
+        'add': a + b,
+        'subtract': a - b,
+        'multiply': a * b,
+        'divide': a / b,
+    }[operator]
+
+"""
+print(arithmetic(1, 2, "add"))
