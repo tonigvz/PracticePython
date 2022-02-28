995ab651775bcbdb0c450105a4d39b2f0392c866
Antonia <goagaantonia@gmail.com>
2022-02-28 16:30:34 +0200

    laptop/sortbylength

---
+++ solutions/sortarraybystringlength/sortarraystringlength.py
@@ -0,0 +1,6 @@
+#  https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c
+def sort_by_length(arr):
+    return sorted(arr, key=len)
+
+
+print(sort_by_length(["Telescopes", "Glasses", "Eyes", "Monocles"]))
